b77eb909089f20adc29b782d714cd7cd8c74016d
sejalpatial <sejalpatial29@gmail.com>
2026-01-13 23:36:25 +0530

    edited

--- EXPT1.sql
+++ EXPT1.sql
@@ -9,24 +9,25 @@ CREATE TABLE MEMBERS(
 uid NUMERIC PRIMARY KEY,
 member_name varchar (50)
 );
+
 create table book_issue(
 member_uid numeric not null,
 book_id numeric not null,
 date_of_issue date not null,
 foreign key (member_uid) references members(uid),
 foreign key (book_id) references book(book_id)
-)
+);
 INSERT INTO book (book_id,book_name,book_author,date_of_issue)
 VALUES('1000','HARRY POTTER','JK ROWLING','12-10-26'),
-('1001','MAGIC','ANNIE HATHAWAY','11-10-22'),
-('1002','ATOMIC HABITS','ABC','11-10-20');
+('1001','MAGIC','RHONDA BYRNE','11-10-22'),
+('1002','ATOMIC HABITS','JAMES CLEAR','11-10-20');
 
 SELECT * FROM book; 
 
-INSERT INTO MEMBERS (member_uid,member_name)
-VALUES('202','RAJU'),
+INSERT INTO MEMBERS (uid,member_name)
+VALUES('202','RAJ'),
 ('203','TOMMY'),
-('204','TILLU');
+('204','jerry');
 
 SELECT * FROM MEMBERS;
 
@@ -34,15 +35,20 @@ INSERT INTO book_issue (member_uid,book_id,date_of_issue)
 VALUES('202','1000','12-6-25'),
 ('203','1000','18-09-25'),
 ('204','1001','20-10-24');
-UPDATE MEMBERS SET member_name='yoma' where member_name='RAJU'; 
+DELETE FROM MEMBERS WHERE member_name='jerry';
+SELECT * FROM MEMBERS;
+
+UPDATE MEMBERS SET member_name='yoma' where member_name='RAJ'; 
 SELECT * FROM book_issue;
 
-CREATE ROLE Librarian;
-CREATE ROLE Librarian3 WITH LOGIN PASSWORD 'lib123';
+CREATE ROLE Librarian
+WITH LOGIN PASSWORD 'lib123'
+
+SELECT CURRENT_USER;
 
 
-GRANT SELECT,INSERT,DELETE ON MEMBERS,book_issue,book to Librarian;
-GRANT Librarian1 TO Sejal;
+GRANT SELECT,INSERT,DELETE,UPDATE ON table MEMBERS,book_issue,book to Librarian;
+REVOKE SELECT,INSERT,DELETE,UPDATE ON table MEMBERS,book_issue,book from Librarian;
 
 
 
