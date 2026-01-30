b2351e9b562700209f8602685d8780220cc124bb
sejalpatial <sejalpatial29@gmail.com>
2026-01-30 23:45:51 +0530

    added leetcode problem

--- Experiment_2/Experiment_2.sql
+++ Experiment_2/Experiment_2.sql
@@ -1,4 +1,4 @@
-
+-- Step 1: Create EMPLOYEE t
 CREATE TABLE employee (
     emp_id        NUMERIC PRIMARY KEY,
     emp_name      VARCHAR(50),
@@ -7,16 +7,16 @@ CREATE TABLE employee (
     joining_date  DATE
 );
 
-
+-- Step 2: Insert sample records
 INSERT INTO employee (emp_id, emp_name, department, salary, joining_date) VALUES
-(1,'Amit','HR',25000,'2021-06-15'),
-(2,'Riya','HR',35000, '2020-03-10'),
-(3,'Suresh', 'IT',45000, '2019-01-25'),
-(4,'Neha','IT',30000, '2022-07-18'),
-(5,'Karan','Finance',20000,'2021-11-05'),
-(6,'Pooja','Finance',55000,'2018-09-12'),
-(7,'Rahul','Sales',28000,'2020-02-20'),
-(8,'Anita','Sales',42000,'2019-08-30');
+(1, 'Amit',   'HR',        25000, '2021-06-15'),
+(2, 'Riya',   'HR',        35000, '2020-03-10'),
+(3, 'Suresh', 'IT',        45000, '2019-01-25'),
+(4, 'Neha',   'IT',        30000, '2022-07-18'),
+(5, 'Karan',  'Finance',   20000, '2021-11-05'),
+(6, 'Pooja',  'Finance',   55000, '2018-09-12'),
+(7, 'Rahul',  'Sales',     28000, '2020-02-20'),
+(8, 'Anita',  'Sales',     42000, '2019-08-30');
 
 SELECT department,
        AVG(salary)::NUMERIC(10,2) AS avg_salary
@@ -46,7 +46,5 @@ WHERE salary > 20000
 GROUP BY department
 HAVING AVG(salary) > 30000
 ORDER BY avg_salary DESC;
-
-
 
 
